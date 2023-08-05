25e4f21a5ebf41bb36b8fe45c2cac2a4d74a5263
jsejas <jsejas@nosis.com>
2023-08-05 14:17:02 -0300

    feat: add vue app

---
+++ juliosm/src/components/Terminal.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main id="container">
+    <div id="terminal">
+        <!-- Terminal Bar -->       
+        <section id="terminal__bar">          
+        <div id="bar__buttons">            
+            <button class="bar__button" id="bar__button--exit">&#10005;</button>            
+            <button class="bar__button">&#9472;</button>                
+            <button class="bar__button">&#9723;</button>          
+        </div>          
+        <p id="bar__user">root@jsm: ~</p>        
+        </section>        
+        <!-- Terminal Body -->        
+        <section id="terminal__body">          
+        <div id="terminal__prompt">            
+            <span id="terminal__prompt--user">root@jsm:</span>            
+            <span id="terminal__prompt--location">~</span>            
+            <span id="terminal__prompt--bling">$ under construction...</span>            
+            <span id="terminal__prompt--cursor"></span>          
+        </div>        
+        </section>      
+    </div>    
+    </main>
+</template>
