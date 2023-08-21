abdf151759f071f7b30e0df7b7c193d8f1596237
julioSM98 <juliosejas98@gmail.com>
2023-08-21 19:24:55 -0300

    fix: title in a and img

--- juliosm/src/components/Icons.vue
+++ juliosm/src/components/Icons.vue
@@ -4,28 +4,28 @@
 <template>
    <div id="--icons">
         <div class="--li">
-            <a href="https://www.linkedin.com/in/juliosm/"> 
-                <img src="../assets/linkedin.svg" alt="linkedin">
+            <a title="linkedin" href="https://www.linkedin.com/in/juliosm/" target="_blank"> 
+                <img src="../assets/linkedin.svg" alt="linkedin" title="linkedin">
             </a>
         </div>
         <div class="--li">
-            <a href="mailto: juliosejas98@gmail.com">
-                <img src="../assets/mail.svg" alt="mail">
+            <a title="mail" href="mailto: juliosejas98@gmail.com" target="_blank">
+                <img src="../assets/mail.svg" alt="mail" title="mail">
             </a>
         </div>
         <div class="--li">
-            <a href="https://cdn.jsdelivr.net/gh/JulioSM98/juliosm98.github.io@main/attachment/CV-JulioSejas.pdf" target="_blank">
-                <img src="../assets/cv.svg" alt="cv">
+            <a title="cv" href="https://cdn.jsdelivr.net/gh/JulioSM98/juliosm98.github.io@main/attachment/CV-JulioSejas.pdf" target="_blank">
+                <img src="../assets/cv.svg" alt="cv" title="cv">
             </a>
         </div>
         <div class="--li">
-            <a href="https://www.instagram.com/juliosm.zip/">
-                <img src="../assets/instagram.svg" alt="instagram">
+            <a title="instagram" href="https://www.instagram.com/juliosm.zip/" target="_blank">
+                <img src="../assets/instagram.svg" title="instagram" alt="instagram">
             </a>
         </div>
         <div class="--li">
-            <a href="https://github.com/JulioSM98">
-                <img src="../assets/github.svg" alt="github">
+            <a title="github" href="https://github.com/JulioSM98" target="_blank">
+                <img title="github" src="../assets/github.svg" alt="github">
             </a>
         </div>
    </div> 
